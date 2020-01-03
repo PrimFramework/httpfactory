a3adfd4ef93e88f23e168a10322bc195909cf8ce
Kendall Weaver <kendall@ringfree.com>
2020-01-03 14:36:27 -0500

    Add files.

---
+++ src/HttpFactory.php
@@ -0,0 +1,246 @@
+<?php
+
+/**
+ * PSR-17 factory for creating objects implementing the PSR-7 interfaces. Also
+ * has functions for returning the implementation class names and function for
+ * PSR-18 HTTP clients as well.
+ *
+ * This is mostly ripped straight out of the 2.x dev branch of guzzlehttp/psr7.
+ *
+ * @author Kendall Weaver <kendalltweaver@gmail.com>
+ * @since 0.0.1 Initial Release
+ */
+
+declare(strict_types=1);
+
+namespace Prim\HttpFactory;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\Stream;
+use GuzzleHttp\Psr7\UploadedFile;
+use GuzzleHttp\Psr7\Uri;
+use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriInterface;
+use SocialConnect\HttpClient\Curl;
+
+class HttpFactory implements
+    RequestFactoryInterface,
+    ResponseFactoryInterface,
+    ServerRequestFactoryInterface,
+    StreamFactoryInterface,
+    UploadedFileFactoryInterface,
+    UriFactoryInterface
+{
+
+    /**
+     * @return EmitterInterface
+     */
+    public function createEmitter(): EmitterInterface
+    {
+        return new SapiEmitter;
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function emitterClass(): string
+    {
+        return SapiEmitter::class;
+    }
+
+
+    /**
+     * @return ClientInterface
+     */
+    public function createClient(): ClientInterface
+    {
+        return new Curl;
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function clientClass(): string
+    {
+        return Curl::class;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createUploadedFile(
+        StreamInterface $stream,
+        int $size = null,
+        int $error = \UPLOAD_ERR_OK,
+        string $clientFilename = null,
+        string $clientMediaType = null
+    ): UploadedFileInterface {
+        if ($size === null) {
+            $size = $stream->getSize();
+        }
+
+        return new UploadedFile($stream, $size, $error, $clientFilename, $clientMediaType);
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function uploadedFileClass(): string
+    {
+        return UploadedFile::class;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createStream(string $content = ''): StreamInterface
+    {
+        return \GuzzleHttp\Psr7\stream_for($content);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createStreamFromFile(string $file, string $mode = 'r'): StreamInterface
+    {
+        try {
+            $resource = \GuzzleHttp\Psr7\try_fopen($file, $mode);
+        } catch (\RuntimeException $e) {
+            if ('' === $mode || false === \in_array($mode[0], ['r', 'w', 'a', 'x', 'c'], true)) {
+                throw new \InvalidArgumentException(sprintf('Invalid file opening mode "%s"', $mode), 0, $e);
+            }
+
+            throw $e;
+        }
+
+        return \GuzzleHttp\Psr7\stream_for($resource);
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createStreamFromResource($resource): StreamInterface
+    {
+        return \GuzzleHttp\Psr7\stream_for($resource);
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function streamClass(): string
+    {
+        return Stream::class;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createServerRequest(string $method, $uri, array $serverParams = []): ServerRequestInterface
+    {
+        if (empty($method)) {
+            if (!empty($serverParams['REQUEST_METHOD'])) {
+                $method = $serverParams['REQUEST_METHOD'];
+            } else {
+                throw new \InvalidArgumentException('Cannot determine HTTP method');
+            }
+        }
+
+        return new ServerRequest($method, $uri, [], null, '1.1', $serverParams);
+    }
+
+
+    /**
+     * @return ServerRequestInterface
+     */
+    public function createServerRequestFromGlobals(): ServerRequestInterface
+    {
+        return ServerRequest::fromGlobals();
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function serverRequestClass(): string
+    {
+        return ServerRequest::class;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
+    {
+        return new Response($code, [], null, '1.1', $reasonPhrase);
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function responseClass(): string
+    {
+        return Response::class;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createRequest(string $method, $uri): RequestInterface
+    {
+        return new Request($method, $uri);
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function requestClass(): string
+    {
+        return Request::class;
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createUri(string $uri = ''): UriInterface
+    {
+        return new Uri($uri);
+    }
+
+
+    /**
+     * @return string
+     */
+    public static function uriClass(): string
+    {
+        return Uri::class;
+    }
+}
